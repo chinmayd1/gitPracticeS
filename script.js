7610569e52c42fd46a6d2fbc92bbdc69aa0236b7
chinmayd1 <chinmay.minskole@gmail.com>
2026-02-28 09:20:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello 9 am session")
+
+// echo "# gitPracticeS" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/chinmayd1/gitPracticeS.git
+// git push -u origin main
